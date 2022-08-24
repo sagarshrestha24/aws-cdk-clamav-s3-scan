f85a4fbd4c716e34e8e13ace2cba1d81e886aec7
dipendrachaudhary <dipendrac3@gmail.com>
2022-08-24 20:48:55 +0545

    Initial commit

---
+++ test/clamav-test.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ClamavTest from '../lib/clamav-test-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/clamav-test-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ClamavTest.ClamavTestStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
